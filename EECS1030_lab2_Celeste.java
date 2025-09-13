6b3924dc666f4dc0ab37d274dbc10dd2ed1da59a
celestehernandez19 <celestethernandez@gmail.com>
2025-09-12 20:09:43 -0400

    java file

---
+++ EECS1030_lab2_Celeste.java
@@ -0,0 +1,10 @@
+//Created by Celeste Hernandez 2025-09-10
+
+package EECS1030_lab2_Celeste;
+
+public class EECS1030_lab2_Celeste { 
+    public static void main (String[] args){
+        System.out.println(x:"Hello, World!");
+    }
+
+}
